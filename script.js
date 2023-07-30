9ba5719e8ef7e052cba853fadf8baac403f4caac
Manthankk <98227419+Manthankk@users.noreply.github.com>
2023-07-31 00:59:23 +0530

    Add files

---
+++ script.js
@@ -0,0 +1,73 @@
+let words= document.querySelectorAll(".word");
+words.forEach((word)=>{
+    let letters=word.textContent.split("");
+    word.textContent="";
+    letters.forEach((letter)=>{
+        let span=document.createElement("span");
+        span.textContent=letter;
+        span.className="letter";
+        word.append(span);
+    });
+});
+
+let currentWordIndex=0;
+let maxWordIndex=words.length -1;
+words[currentWordIndex].style.opacity ="1";
+
+let changeText=()=>{
+    let currentWord=words[currentWordIndex];
+    let nextWord=currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex+1];
+
+    Array.from(currentWord.childern).forEach((letter,i)=>{
+        setTimeout(()=>{
+            letter.className="letter out";
+
+        },i*80);
+    });
+    nextWord.style.opacity="1"; 
+    Arrey.from(nextWord.childern).forEach((letter,i)=>{
+        letter.className="letter behind";
+        setTimeout(()=>{
+            letter.className="letter in";
+        
+        },340+i*80 );
+    });
+    currentWordIndex = currentWordIndex=== maxWordIndex ? 0 : currentWordIndex+1;
+
+};
+changeText();
+setInterval(changeText,3000);
+
+//////// Circle Skills ////////////////////////
+
+// const circles= document.querySelectorAll('.circle');
+// circles.forEach(elem=>{
+//     var dots =elem.getAttribute("data-dots");
+//     var marked =elem.getAttribute("data-percent"); 
+//     var percent=Math.floor(dots*marked/100);
+//     var points="";
+//     var rotate=360/ dots;
+
+//     for(let i=0;i<dots;i++){
+//         points +=    <div class="points" style="--i:${i};--rot:${rotate}deg"></div> ;}
+// elem.innerHTML=points;
+// })
+
+const circles = document.querySelectorAll('.circle');
+circles.forEach(elem => {
+    var dots = elem.getAttribute("data-dots");
+    var marked = elem.getAttribute("data-percent");
+    var percent = Math.floor(dots * marked / 100);
+    var points = "";
+    var rotate = 360 / dots;
+
+    for (let i = 0; i < dots; i++) {
+        points += <div class="points" style="--i:${i};--root:${rotate *i}deg"></div>;
+    }
+    elem.innerHTML = points;
+
+    const pointsMarked =elem.querySelectorAll('.point');
+    for(let i=0;i<percent;i++){
+        pointsMarked[i].classList.add('marked')
+    }
+});
